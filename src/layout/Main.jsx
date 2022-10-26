d70e8f321b07a21b5722a06c4559fd3e1f8c5b69
rustam9724 <rustam_9724@mail.ru>
2022-10-26 15:37:18 +0500

    init: header adaptive

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -15,6 +15,10 @@ class Main extends React.Component {
         fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=zootopia`)
             .then(response => response.json())
             .then(data => this.setState({movies: data.Search, loading: false}))
+            .catch(err => {
+                console.log(err);
+                this.setState({loading: false});
+            })
     }
 
     searchMovies = (str, type = 'all') => {
