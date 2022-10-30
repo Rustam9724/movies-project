14369e24c272c0a0c6da43c85d00edf19f444e77
rustam9724 <rustam_9724@mail.ru>
2022-10-30 10:56:31 +0500

    refactor: converting the Main component from class to function

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,43 +1,64 @@
-import React from "react"
+import React, { useState, useEffect }from "react"
 import {Movies} from "../components/Movies";
 import {Preloader} from '../components/Preloader';
 import {Search} from '../components/Search';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-class Main extends React.Component {
-    state = {
-        movies: [],
-        loading: true
+function Main() {
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    // state = {
+    //     movies: [],
+    //     loading: true
+    // }
+
+    function searchMovies(str, type = 'all') {
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+        .then(response => response.json())
+        .then(data => {
+            setMovies(data.Search);
+            setLoading(false);
+        });
     }
 
-    componentDidMount() {
+    useEffect(() => {
         fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=zootopia`)
             .then(response => response.json())
-            .then(data => this.setState({movies: data.Search, loading: false}))
+            .then(data => {
+                setMovies(data.Search);
+                setLoading(false);
+            })
             .catch(err => {
                 console.log(err);
-                this.setState({loading: false});
+                setLoading(false);
             })
-    }
+    }, []) 
 
-    searchMovies = (str, type = 'all') => {
-        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
-            .then(response => response.json())
-            .then(data => this.setState({movies: data.Search, loading: false}));
-    }
+    // componentDidMount() {
+    //     fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=zootopia`)
+    //         .then(response => response.json())
+    //         .then(data => this.setState({movies: data.Search, loading: false}))
+    //         .catch(err => {
+    //             console.log(err);
+    //             this.setState({loading: false});
+    //         })
+    // }
 
-    render() {
-        const {movies, loading} = this.state;
+    // searchMovies = (str, type = 'all') => {
+    //     fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+    //         .then(response => response.json())
+    //         .then(data => this.setState({movies: data.Search, loading: false}));
+    // }
 
         return <main className="container content"> 
-            <Search searchMovies={this.searchMovies}/>
+            <Search searchMovies={searchMovies}/>
             {
                 loading ? <Preloader /> : 
                 <Movies movies={movies}/>
             }
         </main>
-    }
 }
 
 export {Main}
